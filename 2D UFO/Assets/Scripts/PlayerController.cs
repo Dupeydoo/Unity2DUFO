5229cec996492f7643459ebf02687b7bbf006a15
Dupeydoo <jrmd202@exeter.ac.uk>
2016-11-10 20:40:48 +0000

    Overlapping collection of pickups and 2d optimise

--- 2D UFO/Assets/Scripts/PlayerController.cs
+++ 2D UFO/Assets/Scripts/PlayerController.cs
@@ -20,4 +20,10 @@ public class PlayerController : MonoBehaviour {
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rgb2d.AddForce(movement * speed);
     }
+
+    internal void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+            other.gameObject.SetActive(false);
+    }
 }
