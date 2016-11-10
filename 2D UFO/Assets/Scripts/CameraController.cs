b558bee4a8da5460fdd29454acc9dbb671450306
Dupeydoo <jrmd202@exeter.ac.uk>
2016-11-10 19:12:34 +0000

    Physics, player, collision areas, movement, rotating pickups and reusable prefabs

---
+++ 2D UFO/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	internal void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	internal void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ 2D UFO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rgb2d;
+    public float speed;
+
+    internal void Start()
+    {
+        rgb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+	internal void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rgb2d.AddForce(movement * speed);
+    }
+}
