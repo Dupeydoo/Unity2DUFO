ada68fc00dee5efc4c88148a4d26aea3cccaa706
Dupeydoo <jrmd202@exeter.ac.uk>
2016-11-11 16:00:15 +0000

    Collections and win conditions

--- 2D UFO/Assets/Scripts/PlayerController.cs
+++ 2D UFO/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rgb2d;
+    private int count;
     public float speed;
+    public Text countText;
+    public Text winText;
 
     internal void Start()
     {
         rgb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
 
@@ -24,6 +31,17 @@ public class PlayerController : MonoBehaviour {
     internal void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("PickUp"))
+        {
             other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+            winText.text = "You Win!";
     }
 }
